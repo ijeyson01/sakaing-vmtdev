87c2e439a63bc8cf12ec42e163514995e1dffe38
Jeyson Jaramillo <jjaramillo@viamatica.com>
2025-02-26 21:05:41 -0500

    Tabla de plantilla para productos

--- src/app/business/product/product.component.ts
+++ src/app/business/product/product.component.ts
@@ -1,8 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { RatingModule } from 'primeng/rating';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [
+    CommonModule,
+    ToolbarModule,
+    ButtonModule,
+    TableModule,
+    IconFieldModule,
+    InputIconModule,
+    RatingModule,
+    FormsModule,
+    TagModule,
+    InputTextModule
+  ],
   templateUrl: './product.component.html',
   styles: ``
 })
