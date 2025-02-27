589e92beb864fe7a58fb7d2bc1ff0ba7cbc87176
Jeyson Jaramillo <jjaramillo@viamatica.com>
2025-02-26 20:50:05 -0500

    Creacion de nuevo componente

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -14,7 +14,8 @@ export const appRoutes: Routes = [
             { path: '', component: Dashboard },
             { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
             { path: 'documentation', component: Documentation },
-            { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
+            { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') },
+            { path: 'business', loadChildren: () => import('./app/business/business.routes') }
         ]
     }, //guard
     { path: 'landing', component: Landing },
---
+++ src/app/business/business.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { ProductComponent } from "./product/product.component";
+
+export default [
+    { path: 'products', component: ProductComponent }
+] as Routes;
