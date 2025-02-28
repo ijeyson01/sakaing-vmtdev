ab75210e69ca599ff561df7cf0f850bd7e78eb7a
Jeyson Jaramillo <jjaramillo@viamatica.com>
2025-02-27 19:40:02 -0500

    Componente categoria

--- src/app/business/business.routes.ts
+++ src/app/business/business.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from "@angular/router";
 import { ProductComponent } from "./product/product.component";
+import { CategoryComponent } from "./category/category.component";
 
 export default [
-    { path: 'products', component: ProductComponent }
+    { path: 'products', component: ProductComponent },
+    { path: 'category', component: CategoryComponent}
 ] as Routes;
